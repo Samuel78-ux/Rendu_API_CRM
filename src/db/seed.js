4a4e0009201867c74965e81d7657c8f21e3024bb
Samuel78-ux <70773349+Samuel78-ux@users.noreply.github.com>
2023-03-05 19:00:21 +0100

    modify

--- src/db/seed.js
+++ src/db/seed.js
@@ -1,3 +1,17 @@
-import { faker } from "@faker-js/faker"
 import knex from "knex"
 import config from "../config.js"
+import hashPassword from "../hashPassword.js"
+
+const db = knex(config.db)
+
+const [passwordHash, passwordSalt] = hashPassword("samuelcamara78")
+await db("users").insert([
+  {
+    firstName: "Samuel",
+    lastName: "Camara",
+    email: "samu@gmail.com",
+    roleId: 1,
+    passwordHash: passwordHash,
+    passwordSalt: passwordSalt,
+  },
+])
